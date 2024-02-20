6784999041b5b27617f0f90cd08d5b745b6dafa5
franciscolg13 <146923665+franciscolg13@users.noreply.github.com>
2024-02-20 19:52:27 +0100

    Add files via upload

--- src/app.py
+++ src/app.py
@@ -141,7 +141,7 @@ def compile_stats(games, goals, assists, pens_att, pens_made, progressive_carrie
         
       
 translator = Translator()
-# language = "español"
+language = "inglés"
 
 def translate(text):
     global language
