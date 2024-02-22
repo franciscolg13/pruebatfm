6bf5d87f832a346f32d38e03af5e62767d140d54
franciscolg13 <146923665+franciscolg13@users.noreply.github.com>
2024-02-22 17:19:47 +0100

    Add files via upload

--- src/app.py
+++ src/app.py
@@ -10,15 +10,16 @@ modelo_ruta = 'model/xg_model_decision_tree_regressor.pkl'
 xg_model_decision_tree_regressor = joblib.load(modelo_ruta)
 
 st.markdown(
-    """
+    f"""
     <style>
-        body {
+        body {{
             background-color: #12EAAF;
-        }
+        }}
     </style>
     """,
     unsafe_allow_html=True
 )
+
 
 def contiene_solo_letras(cadena):
     return all(caracter.isalpha() or caracter.isspace() for caracter in cadena)
