d048375c8b27346ea813c5cb7309977cabcf4f6c
franciscolg13 <146923665+franciscolg13@users.noreply.github.com>
2024-02-22 19:04:49 +0100

    Add files via upload

--- src/app.py
+++ src/app.py
@@ -212,15 +212,14 @@ with col2:
 
 
 select_language_msg = translate("Selecciona el idioma: ")
-spanish_button = st.button(translate("Español"))
-english_button = st.button(translate("Inglés"))
+spanish_option = translate("Español")
+english_option = translate("Inglés")
 
-if spanish_button:
-    st.session_state["language"] = "español"
-elif english_button:
-    st.session_state["language"] = "inglés"
-
-language = st.session_state.get("language", "inglés")  # Default to English if not set
+if st.sidebar.button("Español"):
+    language = "español"
+elif st.sidebar.button("Inglés"):
+    language = "inglés"
+# 
 # if option == spanish_option:
 #     language = "español"
 # elif option == english_option:
