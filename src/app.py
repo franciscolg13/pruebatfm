1f315fa74b57f6233f63a8a962a0d1d7de2f6866
franciscolg13 <146923665+franciscolg13@users.noreply.github.com>
2024-02-22 17:46:05 +0100

    Add files via upload

--- src/app.py
+++ src/app.py
@@ -14,19 +14,17 @@ ruta_imagen_local_pelota = os.path.join("media", "logo_pelota.png")
 
 st.set_page_config(
     page_icon=ruta_imagen_local_pelota,
+    # page_title="ExpectedFoot"
 )
 
-page = f"""
+page = """
 <style>
-    img[alt="ExpectedFoot"] {{
-        background-color: #169E79;
-        border-radius: 10px;
-    }}
+[data-testid=stAppViewContainer]{
+    background-color: #169E79;
+    border-radius: 10px;
+}
 </style>
 """
-
-
-
 
 st.markdown(page, unsafe_allow_html=True)
 
