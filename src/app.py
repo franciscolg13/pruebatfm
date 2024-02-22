c6ba209d2e65cbdf13b92849f598c71c4a1c342d
franciscolg13 <146923665+franciscolg13@users.noreply.github.com>
2024-02-22 18:37:59 +0100

    Add files via upload

--- src/app.py
+++ src/app.py
@@ -216,7 +216,7 @@ english_option = translate("Inglés")
 
 option = st.sidebar.radio("Seleccionar idioma: ", (spanish_option, english_option),
                           key='select_language', label_visibility="hidden",
-                          format_func=lambda x: f'<div class="radio-button-container">{x}</div>')
+                          format_func=lambda x: f'{x}')
 if option == spanish_option:
     language = "español"
 elif option == english_option:
